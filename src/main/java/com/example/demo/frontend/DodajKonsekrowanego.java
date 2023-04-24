011b3796e26bad73a840fb762f68dd16bfc77903
Arkadiusz Galus <arkadiuszgalus85@gmail.com>
2023-04-25 01:46:05 +0200

    switching between routes

--- src/main/java/com/example/demo/frontend/DodajKonsekrowanego.java
+++ src/main/java/com/example/demo/frontend/DodajKonsekrowanego.java
@@ -4,6 +4,8 @@ import com.example.demo.entity.Konsekrowany;
 import com.example.demo.repository.Repo;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -17,12 +19,23 @@ public class DodajKonsekrowanego extends VerticalLayout {
 
     @Autowired
     public DodajKonsekrowanego(Repo repository){
+
+
+
+        var homeMenu = new Button("Menu Główne");
+        homeMenu.setIcon(new Icon(VaadinIcon.HOME));
+
+        homeMenu.addClickListener(buttonClickEvent -> {
+            homeMenu.getUI().ifPresent(ui -> ui.navigate(""));
+
+        });
         TextField textFieldImie = new TextField("imie");
         TextField textFieldNazwisko = new TextField("Nazwisko");
         TextField textFielOaza = new TextField("Oaza");
         DatePicker picker = new DatePicker("Podaj date urodzenia");
         picker.setWeekNumbersVisible(true);
         Button buttonDodaj = new Button("Dodaj Konsekrowanego");
+
 
         buttonDodaj.addClickListener(buttonClickEvent -> {
             Konsekrowany konsekrowany = new Konsekrowany();
@@ -37,7 +50,15 @@ public class DodajKonsekrowanego extends VerticalLayout {
 
         });
 
-        add(textFieldImie,textFieldNazwisko,textFielOaza,picker,buttonDodaj);
+
+//
+//        znajdz.addClickListener(buttonClickEvent -> {
+//            znajdz.getUI().ifPresent(ui -> ui.navigate("Znajdz konsekrowanego"));
+//        });
+
+
+
+        add(textFieldImie,textFieldNazwisko,textFielOaza,picker,buttonDodaj,homeMenu);
 
 
 
