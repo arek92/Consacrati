5accfb0d63fe22f172c5674cbae851e5c9230e03
Arkadiusz Galus <arkadiuszgalus85@gmail.com>
2023-04-26 01:40:01 +0200

    adding image to application

--- src/main/java/com/example/demo/frontend/MainView.java
+++ src/main/java/com/example/demo/frontend/MainView.java
@@ -1,10 +1,14 @@
 package com.example.demo.frontend;
 
+import com.example.demo.ImageClassResourcess;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.TabVariant;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.PermitAll;
@@ -17,32 +21,35 @@ public class MainView extends Div {
 
      public MainView(){
 
-         Tab dodajKonsekrowanego = new Tab("Dodaj konsekrowanego");
-         Tab wyszukajKonsekrowanegoLubInformacje = new Tab("znajdz konsekrowanego");
-         Tab czyKtosMaUrodziny = new Tab("komu zyczyc dzisiaj 100 lat");
-         Tab aktualizujInformacje = new Tab("Assetti- zmienmy dane");
 
+
+         Tab dodajKonsekrowanego = new Tab(VaadinIcon.USER.create(), new Span("Dodaj"));
+         Tab znajdzInformacje = new Tab(VaadinIcon.SEARCH.create(), new Span("Szukaj"));
+         Tab urodziny = new Tab(VaadinIcon.CALENDAR_CLOCK.create(), new Span("Urodziny"));
+         Tab aktualizujInformacje = new Tab(VaadinIcon.EDIT.create(), new Span("Zaktualizuj"));
+
+         var ressources = new ImageClassResourcess();
+
+         // Set the icon on top
+         for (Tab tab : new Tab[] { dodajKonsekrowanego, znajdzInformacje, urodziny,aktualizujInformacje}) {
+             tab.addThemeVariants(TabVariant.LUMO_ICON_ON_TOP);
+         }
 
          dodajKonsekrowanego.getElement().addEventListener("click",attachEvent ->
                  dodajKonsekrowanego.getUI().ifPresent(ui -> ui.navigate("Dodaj_Konsekrowanego")));
 
-         wyszukajKonsekrowanegoLubInformacje.getElement().addEventListener("click",attachEvent ->
-                 wyszukajKonsekrowanegoLubInformacje.getUI().ifPresent(ui -> ui.navigate("Znajdz konsekrowanego")));
+         znajdzInformacje.getElement().addEventListener("click",attachEvent ->
+                 znajdzInformacje.getUI().ifPresent(ui -> ui.navigate("Znajdz konsekrowanego")));
 
-         czyKtosMaUrodziny.getElement().addEventListener("click",attachEvent ->
-                 czyKtosMaUrodziny.getUI().ifPresent(ui -> ui.navigate("Czy ktos ma urodziny")));
+         urodziny.getElement().addEventListener("click",attachEvent ->
+                 urodziny.getUI().ifPresent(ui -> ui.navigate("Czy ktos ma urodziny")));
 
          aktualizujInformacje.getElement().addEventListener("click",attachEvent ->
                  aktualizujInformacje.getUI().ifPresent(ui -> ui.navigate("zaktualizuj dane")));
 
 
-
-
-
-
-
-         Tabs tabs = new Tabs(dodajKonsekrowanego,wyszukajKonsekrowanegoLubInformacje,czyKtosMaUrodziny,aktualizujInformacje);
-         add(tabs);
+         Tabs tabs = new Tabs(dodajKonsekrowanego,znajdzInformacje,urodziny,aktualizujInformacje);
+         add(tabs,ressources);
 
 
 
